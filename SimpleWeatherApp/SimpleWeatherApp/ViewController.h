b43237e649f78d15215a74c4a885f7a10a6de0ef
Alex Golub <alexeythumb@gmail.com>
2016-01-09 14:49:40 +0200

    Added default project

---
+++ SimpleWeatherApp/SimpleWeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleWeatherApp
+//
+//  Created by Alex Golub on 1/9/16.
+//  Copyright © 2016 Alex Golub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

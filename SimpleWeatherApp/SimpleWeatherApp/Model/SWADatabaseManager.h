87bdc24f4a27173709b47764653722adfd2f5979
Alex Golub <alexeythumb@gmail.com>
2016-01-10 19:14:07 +0200

    Added DatabaseManager, Core Data stack moved

---
+++ SimpleWeatherApp/SimpleWeatherApp/Model/SWADatabaseManager.h
@@ -0,0 +1,24 @@
+//
+//  SWADatabaseManager.h
+//  SimpleWeatherApp
+//
+//  Created by Alex Golub on 1/10/16.
+//  Copyright © 2016 Alex Golub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SWADatabaseManager : NSObject
+
++ (SWADatabaseManager *)sharedInstance;
+
+/// Core Data Stack
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, strong) NSManagedObjectContext *backgroundManagedObjectContext;
+@property (nonatomic, strong, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, strong, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end

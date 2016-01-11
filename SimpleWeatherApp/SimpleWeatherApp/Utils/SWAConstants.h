b073306f4c888f49580666cb4805fb53654ad8a8
Alex Golub <alexeythumb@gmail.com>
2016-01-11 15:29:35 +0200

    Added Constants

---
+++ SimpleWeatherApp/SimpleWeatherApp/Utils/SWAConstants.h
@@ -0,0 +1,24 @@
+//
+//  QVTConstants.h
+//  Qvout
+//
+//  Created by alex on 10.04.14.
+//  Copyright (c) 2014 alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// Animation Speed
+extern const float kAnimationSpeedVeryHigh;
+extern const float kAnimationSpeedHigh;
+extern const float kAnimationSpeedDefault;
+extern const float kAnimationSpeedLow;
+
+// Entity Names
+//extern NSString * const kCityEntityName;
+
+// Web
+extern NSString * const kServerURLAddress;
+extern NSString * const kAuthKey;
+extern NSString * const kContentType;
+extern NSString * const kAccept;
---
+++ SimpleWeatherApp/SimpleWeatherApp/Utils/SWAConstants.m
@@ -0,0 +1,33 @@
+//
+//  SWAConstants.m
+//  SimpleWeatherApp
+//
+//  Created by alex on 11.01.16.
+//  Copyright (c) 2016 alex. All rights reserved.
+//
+
+#import "SWAConstants.h"
+
+#pragma mark - Animation Speed
+
+const float kAnimationSpeedVeryHigh = 0.1f;
+const float kAnimationSpeedHigh = 0.2f;
+const float kAnimationSpeedDefault = 0.3f;
+const float kAnimationSpeedLow = 0.4f;
+
+#pragma mark - Entity Names
+
+//NSString * const kCityEntityName = @"SWACityDB";
+
+#pragma mark - Web
+
+NSString * const kServerURLAddress = @"http://api.worldweatheronline.com/free/v1/";
+NSString * const kAuthKey = @"3e11ab17a696bfb1ea4714e8d4263";
+NSString * const kContentType = @"application/json";
+NSString * const kAccept = @"application/json";
+
+
+
+
+
+
